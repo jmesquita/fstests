5c23a20771c06ff1de5dfe43aaed32be2a8d0b7f
Joao Mesquita <jmesquita@indicium.com.ar>
2013-01-22 20:17:37 -0300

    Add JSON testing as well.

--- core_tests/__init__.py
+++ core_tests/__init__.py
@@ -1,6 +1,7 @@
 import string
 import random
 from lxml import etree
+import json
 
 from api import APIBase, check_connection_decorator
 from nose.tools import istest
@@ -10,6 +11,10 @@ from nose.tools import istest
 
 class TestShowAPI(APIBase):
     """Test the show API command"""        
+    commands = ['codec', 'endpoint', 'application', 'api', 'dialplan', 'file', 'timer', 'calls', 'channels', 'calls', \
+        'detailed_calls', 'bridged_calls', 'detailed_bridged_calls', 'aliases', 'complete', 'chat', 'management',\
+        'modules', 'nat_map', 'say', 'interfaces', 'interface_types', 'tasks', 'limits', 'status']
+        
     @check_connection_decorator
     def test_show_no_args(self):
         """Test the core show command with no arguments"""
@@ -34,6 +39,17 @@ class TestShowAPI(APIBase):
         except etree.XMLSyntaxError:
             assert False, 'XML Parser failed on\n%s' % e.getBody()
             
+    @check_connection_decorator
+    def show_args_json(self, args):
+        """Test all show commands as JSON and parse it"""
+        e = self.api('show', args)
+        assert (not e.getBody().startswith('-USAGE:'))
+        assert(not e.getBody().startswith('INVALID')), 'FreeSWITCH states this (show %s) is an invalid command!' % (' '.join(args))
+        try:
+            json.loads(e.getBody())
+        except ValueError:
+            assert False, 'JSON Parser failed on\n%s' % e.getBody()
+            
     def test_random_args(self):
         """Test show comamnd with random args"""
         for i in range(10):
@@ -42,8 +58,10 @@ class TestShowAPI(APIBase):
             
     def test_xml_output(self):
         """Test all XML output"""
-        arr = ['codec', 'endpoint', 'application', 'api', 'dialplan', 'file', 'timer', 'calls', 'channels', 'calls', \
-            'detailed_calls', 'bridged_calls', 'detailed_bridged_calls', 'aliases', 'complete', 'chat', 'management',\
-            'modules', 'nat_map', 'say', 'interfaces', 'interface_types', 'tasks', 'limits', 'status']
-        for s in arr:
+        for s in self.commands:
             yield self.show_args_xml,[s, 'as', 'xml']
+            
+    def test_json_output(self):
+        """Test all JSON output"""
+        for s in self.commands:
+            yield self.show_args_json,[s, 'as', 'json']
