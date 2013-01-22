5585bb0c85d12bde5d4cb040893499bd04eccd8b
Joao Mesquita <jmesquita@indicium.com.ar>
2013-01-22 19:36:21 -0300

    Test XML from the show API

--- core_tests/__init__.py
+++ core_tests/__init__.py
@@ -1,7 +1,11 @@
 import string
 import random
+from lxml import etree
+
 from api import APIBase, check_connection_decorator
 from nose.tools import istest
+
+
 
 
 class TestShowAPI(APIBase):
@@ -17,6 +21,14 @@ class TestShowAPI(APIBase):
         """Test the core show command with one argument"""
         e = self.api('show', args)
         assert e.getBody().startswith('-USAGE:')
+        return e
+        
+    @check_connection_decorator
+    def show_args_xml(self, args):
+        """Test all show commands as XML and parse it"""
+        e = self.api('show', args)
+        assert (not e.getBody().startswith('-USAGE:'))
+        etree.fromstring(e.getBody())
         
     @check_connection_decorator
     def test_show_rand_args(self):
@@ -27,4 +39,10 @@ class TestShowAPI(APIBase):
     def test_random_args(self):
         """Test show comamnd with random args"""
         for i in range(10):
-            yield self.show_args, ''.join(random.choice(string.letters) for x in range(5))
+            s = ''.join(random.choice(string.letters) for x in range(5))
+            yield self.show_args, s
+            
+    def test_xml_output(self):
+        """Test all XML output"""
+        for s in ['codec', 'endpoint', 'application', 'api', 'dialplan', 'file', 'timer', 'calls', 'channels', 'calls', 'detailed_calls', 'bridged_calls', 'detailed_bridged_calls', 'aliases', 'complete', 'chat', 'management', 'modules', 'nat_map', 'say', 'interfaces', 'interface_types', 'tasks', 'limits', 'status']:
+            yield self.show_args_xml, [s, 'as', 'xml']
